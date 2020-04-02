7a1f2f327f01ae347fc36627116ee2ec8ef0b890
Boris Bolshakov <boris.bolsh@gmail.com>
2020-04-03 00:00:36 +0300

    Initial commit

---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'sinatra'
+require "sinatra/reloader" if development?
+require 'sqlite3'
+
+get '/' do
+  erb 'Can you handle a secret?'
+end
---
+++ public/scripts.js
@@ -0,0 +1,4 @@
+$(function() {
+    $('select[name="colorpicker"]').simplecolorpicker();
+    $('#datetimepicker').datetimepicker();
+});
