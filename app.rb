33a73708e4d4b0ba550c564c43ee03e99147cc11
Boris Bolshakov <boris.bolsh@gmail.com>
2020-04-03 21:16:15 +0300

    Init database

--- app.rb
+++ app.rb
@@ -3,10 +3,26 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 require 'sqlite3'
 
+def init_db
+  @db = SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash = true 
+end
+
+before do
+  init_db
+end
+
+
 get '/' do
   erb 'Can you handle a secret?'
 end
 
 get '/new' do
   erb :new
+end
+
+post '/new' do
+  content = params[:content]
+
+  erb "You typed #{content}"
 end
